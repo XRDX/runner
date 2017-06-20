505098061e8f703f219c93eb20a3124aa2da7e2b
Vic Wang <305880887@qq.com>
2017-06-20 09:36:51 +0800

    add pixel collision

--- js/collision.js
+++ js/collision.js
@@ -18,7 +18,7 @@ var Collision =  (function(){
 
 	var rect2circle = function(rect, circle){
 		if(rect.x + rect.w < circle.x - circle.r) return false;
-		if(rect.x 		   > circle.x + circle.r) return false;
+		if(rect.x 		     > circle.x + circle.r) return false;
 		if(rect.y + rect.h < circle.y - circle.r) return false;
 		if(rect.y          > circle.y + circle.r) return false;
 
@@ -39,12 +39,64 @@ var Collision =  (function(){
 		if(rect.x + rect.w < line.x1 && rect.w < line.x2) return false;
 		if(rect.y > line.y1 && rect.y > line.y2) return false;
 		if(rect.y + rect.h < line.y1 && rect.y + rect.h < line.y2) return false;
+
+    return true;
+	}
+
+  var getDupRect = function(rect1, rect2){
+    var rect = {};
+    rect.x = Math.max(rect1.x, rect2.x),
+    rect.y = Math.max(rect1.y, rect2.y),
+    rect.w = Math.min(rect1.x + rect1.w, rect2.x + rect2.w) - rect.x;
+    rect.h = Math.min(rect1.y + rect1.h, rect2.y + rect2.h) - rect.y;
+
+    return rect;
+  }
+
+  var canvas1 = document.createElement("canvas");
+  var canvas2 = document.createElement("canvas");
+
+  var ctx1 = canvas1.getContext("2d");
+  var ctx2 = canvas2.getContext("2d");
+
+  canvas1.width = canvas2.width = canvas.width;
+  canvas1.height = canvas2.height = canvas.height;
+
+	var image2image = function(image1, image2){
+		if(rect2rect(image1, image2)){
+
+      ctx1.clearRect(0, 0, canvas.width, canvas.height);
+      ctx2.clearRect(0, 0, canvas.width, canvas.height);
+
+      image1.draw(ctx1);
+      image2.draw(ctx2);
+
+      var rect = getDupRect(image1, image2);
+
+      if(rect.w <= 0 || rect.h <= 0) return false;
+
+      var image1Date = ctx1.getImageData(rect.x, rect.y, rect.w, rect.h);
+      var image2Date = ctx2.getImageData(rect.x, rect.y, rect.w, rect.h);
+
+      var imgData1Data = image1Date.data;
+      var imgData2Data = image2Date.data;
+
+      for(var i = 3, len = imgData1Data.length; i < len; i += 17) {
+        if(imgData1Data[i] && imgData2Data[i]) {;
+          return true;
+        }
+      }
+      return false;
+		}
+
+		return false;
 	}
 
 	var mPublic = {
 		rect2rect: rect2rect,
 		rect2circle: rect2circle,
-		circle2circle: circle2circle
+		circle2circle: circle2circle,
+    image2image: image2image
 	}
 
 	return mPublic;
--- js/shape.js
+++ js/shape.js
@@ -4,7 +4,9 @@ var ShapeTypes = Object.freeze({
   rect: 0,
   circle: 1,
   line: 2,
-  triangle: 3
+  triangle: 3,
+  image: 4,
+  animation: 5
 })
 
 /* Rect */
@@ -63,18 +65,43 @@ FillRect.prototype.draw = function(){
 /* LImage */
 function LImage(img, x, y, w, h){
   Rect.call(this, x, y, w, h);
+  this.type = ShapeTypes.image;
   this.img = img;
 }
 
 LL.inheritPrototype(LImage, Rect);
 
-LImage.prototype.draw = function(){
-  context.drawImage(this.img, this.x, this.y, this.w, this.h) 
+LImage.prototype.draw = function(c){
+  if(c){
+    c.drawImage(this.img, this.x, this.y, this.w, this.h) 
+  } else {
+    context.drawImage(this.img, this.x, this.y, this.w, this.h)    
+  }
+}
+
+LImage.prototype.collide = function(target){
+  switch(target.type)
+  {
+    case ShapeTypes.rect: 
+      return Collision.rect2rect(this, target);
+
+    case ShapeTypes.circle:
+      return Collision.rect2circle(this, target);
+
+    case ShapeTypes.image:
+    case ShapeTypes.animation:
+      return Collision.image2image(this, target);
+
+    default:
+      return false;
+
+  }
 }
 
 /* Animation */
 function Animation(img, x, y, w, h, sw, sh, n, f){
   LImage.call(this, img, x, y, w, h);
+  this.type = ShapeTypes.animation;
   this.frameMax = n;
   this.currentFrame = 0;
   this.sw = sw;
@@ -84,13 +111,19 @@ function Animation(img, x, y, w, h, sw, sh, n, f){
 
 LL.inheritPrototype(Animation, LImage);
 
-Animation.prototype.draw = function(){
+Animation.prototype.draw = function(c){
   var intFrame = Math.floor(this.currentFrame/this.animationFrame);
   var offsetX = this.sw * intFrame;
 
-  context.drawImage(this.img, 
-    offsetX, 0, this.sw, this.sh,
-    this.x, this.y, this.w, this.h);
+  if(c){
+    c.drawImage(this.img, 
+      offsetX, 0, this.sw, this.sh,
+      this.x, this.y, this.w, this.h);    
+  } else {
+    context.drawImage(this.img, 
+      offsetX, 0, this.sw, this.sh,
+      this.x, this.y, this.w, this.h);    
+  }
 
   if(++this.currentFrame >= this.frameMax * this.animationFrame)
     this.currentFrame = 0;
